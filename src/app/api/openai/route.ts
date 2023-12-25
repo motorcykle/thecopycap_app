f886c757f5c126f557a06a617fb296cf655b7ecf
motorcykle <mohamedabdulridha1@gmail.com>
2023-12-25 17:29:14 +0100

    vision route

---
+++ src/app/api/openai/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server"
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI });
+
+export async function POST(request: Request) {
+
+    const data = await request.json();
+
+    if (data?.image) {
+        console.log(data)
+        try {
+            const response = await openai.chat.completions.create({
+                model: "gpt-4-vision-preview",
+                messages: [
+                    {
+                        role: "user",
+                        content: [
+                        { type: "text", text: data?.prompt || "Give me an insta caption based on this image." },
+                        {
+                            type: "image_url",
+                            image_url: {
+                            "url": data.image,
+                            },
+                        },
+                        ],
+                    },
+                ],
+                max_tokens: 2000
+            });
+    
+            console.log(response.choices[0]);
+            return NextResponse.json({ response: response.choices[0]}, { status: 200 })
+        } catch (error) {
+            return NextResponse.json({ error }, { status: 400 })
+        }
+        
+    }
+    
+    return NextResponse.json({ message: 'Received' }, { status: 400 })
+}
